06b2c2122143a9f676391d7ea9c34c82f62d6e55
A.Rumeysa Nur Celik <arncelik@yahoo.com>
2017-02-21 11:21:28 -0600

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,12 @@
+public class Q1{
+	public static void main (String[] args){
+		int count = 3;
+		count++;
+		System.out.println(++count);
+		System.out.println(count);
+		++count;
+		System.out.println(count);
+		System.out.println(count++);
+		System.out.println(count);
+	}
+}
